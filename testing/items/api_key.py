d7b9d67307690efc6549758401bd267c73b39c21
Paul Napolitano <paul.napolitano@yale.edu>
2015-10-06 17:21:23 -0400

    Update api_key.py

--- testing/items/api_key.py
+++ testing/items/api_key.py
@@ -1,1 +1,4 @@
 API_KEY = '1f015596-3c8f-49d4-9de1-1290cbc15a4a'
+
+#NOTE: This key is no longer in service, and for testing purposes 
+#should be replaced with an authorized key found at: https://developer.riotgames.com/
