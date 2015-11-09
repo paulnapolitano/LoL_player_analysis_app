eb5ac5d74c6fa7098db993eda2452da3e36c0212
Paul Napolitano <paul.napolitano@yale.edu>
2015-11-08 19:38:41 -0500

    Add DB population commands
    manage.py commands to populate database with challenger and master
    players' matches

--- mysite/champs/management/commands/populate_challenger.py
+++ mysite/champs/management/commands/populate_challenger.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
+
 from champs.funcs.riot_app import api, RiotException
-from champs.funcs.database_funcs import master_to_db, challenger_to_db
+from champs.funcs.database_funcs import challenger_to_db
 
 class Command(BaseCommand):
     help = 'populates database'
@@ -10,8 +11,3 @@ class Command(BaseCommand):
             challenger_to_db()
         except RiotException:
             print 'Failed to load all challenger players...'
-        
-        try: 
-            master_to_db()
-        except RiotException:
-            print 'Failed to load all master players...'
--- mysite/champs/management/commands/populate_master.py
+++ mysite/champs/management/commands/populate_master.py
@@ -1,16 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
+
 from champs.funcs.riot_app import api, RiotException
-from champs.funcs.database_funcs import master_to_db, challenger_to_db
+from champs.funcs.database_funcs import master_to_db
 
 class Command(BaseCommand):
     help = 'populates database'
     
     def handle(self, *args, **options):
-        try: 
-            challenger_to_db()
-        except RiotException:
-            print 'Failed to load all challenger players...'
-        
         try: 
             master_to_db()
         except RiotException:
